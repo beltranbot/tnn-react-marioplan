5ff1021e28c107c81695dee183d64e7616472b5c
hx <beltranbot@gmail.com>
2018-10-08 21:30:55 -0500

    # 12 - adding dummy data

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,7 +1,13 @@
-const initState = {}
+const initState = {
+  projects: [
+    {id: '1', title: 'help me find peach', content: 'blah blah blah blach'},
+    {id: '2', title: 'collect all the stars', content: 'blah blah blah blach'},
+    {id: '3', title: 'egg hunt with yoshi', content: 'blah blah blah blach'},
+  ]
+}
 
-const projectreducer = (state = initState, action) => {
+const projectReducer = (state = initState, action) => {
   return state
 }
 
-export default projectreducer
+export default projectReducer
