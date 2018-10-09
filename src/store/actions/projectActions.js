95a5549e10975effedc79a561a359ff4b5eedbea
hx <beltranbot@gmail.com>
2018-10-08 22:38:46 -0500

    #18 - adding data to firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,31 @@
 export const createProject = (project) => {
-  return (dispatch, getState, {getFirebase, getFirestore}) => {
+  return async (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async request
-    dispatch({type: 'CREATE_PROJECT', project})
-  }  
+    const firestore = getFirestore()
+    
+    try {
+      await firestore.collection('projects').add({
+        ...project,
+        authorFirstName: 'Net',
+        authorLastName: 'Ninja',
+        authorId: 12345,
+        createdAt: new Date()
+      })
+
+      dispatch({ type: 'CREATE_PROJECT', project })
+    } catch (err) {
+      dispatch({ type: 'CREATE_PROJECT_ERROR', err })
+    }
+    // firestore.collection('projects').add({
+    //   ...project,
+    //   authorFirstName: 'Net',
+    //   authorLastName: 'Ninja',
+    //   authorId: 12345,
+    //   createdAt: new Date()
+    // }).then(() => {
+    //   dispatch({ type: 'CREATE_PROJECT', project })
+    // }).catch((err) => {
+    //   dispatch({ type: 'CREATE_PROJECT_ERROR', err })
+    // })
+  }
 }
