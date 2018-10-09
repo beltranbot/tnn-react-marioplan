4cf4e7d25dd95c576b1f6a6f4d4e4acfef89349a
hx <beltranbot@gmail.com>
2018-10-08 22:47:11 -0500

    # 19 - syncing data with firestore

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,20 +1,22 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 class Dashboard extends Component {
-  render () {
-    const {projects} = this.props
+  render() {
+    const { projects } = this.props
 
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList projects={projects}/>
+            <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1"></div>
-            <Notifications/>
+          <Notifications />
         </div>
       </div>
     )
@@ -22,10 +24,16 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
+  console.log(state)
   return {
-    projects: state.project.projects
+    projects: state.firestore.ordered.projects
   }
 }
 
 
-export default connect(mapStateToProps)(Dashboard)
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'projects' }
+  ])
+)(Dashboard)
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -4,6 +4,7 @@ export const createProject = (project) => {
     const firestore = getFirestore()
     
     try {
+
       await firestore.collection('projects').add({
         ...project,
         authorFirstName: 'Net',
@@ -13,19 +14,9 @@ export const createProject = (project) => {
       })
 
       dispatch({ type: 'CREATE_PROJECT', project })
+
     } catch (err) {
       dispatch({ type: 'CREATE_PROJECT_ERROR', err })
     }
-    // firestore.collection('projects').add({
-    //   ...project,
-    //   authorFirstName: 'Net',
-    //   authorLastName: 'Ninja',
-    //   authorId: 12345,
-    //   createdAt: new Date()
-    // }).then(() => {
-    //   dispatch({ type: 'CREATE_PROJECT', project })
-    // }).catch((err) => {
-    //   dispatch({ type: 'CREATE_PROJECT_ERROR', err })
-    // })
   }
 }
