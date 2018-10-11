76a68cfb69417c9f422724d30c8275d3d187a21e
hx <beltranbot@gmail.com>
2018-10-10 21:34:48 -0500

    # 24 - loggin users out

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -6,10 +6,24 @@ export const signIn = (credentials) => {
         credentials.email,
         credentials.password
       )
-
       dispatch({ type: 'LOGIN_SUCCESS' })
     } catch (err) {
       dispatch({ type: 'LOGIN_ERROR', err })
     }
   }
 }
+
+export const signOut = () => {
+  return async (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase()
+    try {
+      await firebase.auth().signOut()
+
+      dispatch({ type: 'SIGNOUT_SUCCESS' })
+    } catch (error) {
+
+    }
+  }
+
+}
+
