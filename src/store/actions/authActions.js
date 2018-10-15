9b81076866e3607b2dd78f66f21c241cd6a139b2
hx <beltranbot@gmail.com>
2018-10-15 16:49:51 -0500

    # 28 - user sign up (firebase auth)

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -24,6 +24,31 @@ export const signOut = () => {
 
     }
   }
-
 }
 
+export const signUp = (newUser) => {
+  return async (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firebase = getFirebase()
+    const firestore = getFirestore()
+
+    try {
+      let response = await firebase.auth().createUserWithEmailAndPassword(
+        newUser.email,
+        newUser.password
+      )
+
+      await firestore.collection('users').doc(response.user.uid).set({
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        initials: newUser.firstName[0].toUpperCase() + newUser.lastName[0].toUpperCase()
+      })
+
+      dispatch({ type: 'SIGNUP_SUCCESS' })
+    } catch (error) {
+      console.log('error', error)
+      dispatch({type:'SIGNUP_ERROR', error})
+    }
+
+
+  }
+}
