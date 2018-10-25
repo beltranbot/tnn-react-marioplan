f6512dd6907ef8470035c4a9543a884fe83e38e5
hx <beltranbot@gmail.com>
2018-10-24 21:53:52 -0500

    # 37 - cloud function auth trigger and # 38 - showing notifications

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
 
@@ -29,3 +29,17 @@ exports.projectCreated = functions.firestore
 
     return createNotification(notification)
   })
+
+exports.userJoined = functions.auth.user().onCreate((user) => {
+  return admin.firestore().collection('users')
+    .doc(user.uid).get().then((doc) => {
+      const newUser = doc.data()
+      const notification = {
+        content: 'Joined the party',
+        user: `${newUser.firstName} ${newUser.lastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp()
+      }
+      return createNotification(notification)
+    })
+
+})
